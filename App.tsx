09e676ce2e67bbc15b473ec098d2c6ae0ca1a07e
AHSHAKER <ah172002.shaker172002@gmail.com>
2025-05-09 22:32:43 +0300

    Update dependencies and add navigation stack

--- App.tsx
+++ App.tsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
 import {Text, View} from 'react-native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-function App() {
+function HomeScreen() {
   return (
-    <View>
-      <Text>HelloWorld</Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
-export default App;
+
+const Stack = createNativeStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}

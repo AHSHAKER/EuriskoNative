456324d20f69d9ff70694e734db5ec6f72a3d6ce
AHSHAKER <ah172002.shaker172002@gmail.com>
2025-06-05 19:24:17 +0300

    Add react-native-reanimated and react-native-gesture-handler dependencies; update Babel config and wrap App in GestureHandlerRootView

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 /**
  * @format
  */
-
+import 'react-native-reanimated'; // MUST be first
+import 'react-native-gesture-handler'; // also should come early
 import {AppRegistry} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => (
+  <GestureHandlerRootView style={{flex: 1}}>
+    <App />
+  </GestureHandlerRootView>
+);
+
+AppRegistry.registerComponent(appName, () => Root);

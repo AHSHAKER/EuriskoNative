78cd108f7a8427b796fcfbf7de5fefbceb8fab3e
AHSHAKER <ah172002.shaker172002@gmail.com>
2025-06-06 23:18:14 +0300

    Add boot splash screen support; update app name, colors, and styles; integrate react-native-bootsplash

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, {use, useEffect} from 'react';
 import {ThemeProvider} from './src/context/ThemeContext';
 import AppNavigation from './src/navigator/AppNavigation';
 import {enableScreens} from 'react-native-screens';
+import BootSplash from 'react-native-bootsplash';
 enableScreens();
 
 export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      // …do multiple sync or async tasks
+    };
+
+    init().finally(async () => {
+      await BootSplash.hide({fade: true});
+    });
+  }, []);
+
   return (
     <ThemeProvider>
       <AppNavigation />

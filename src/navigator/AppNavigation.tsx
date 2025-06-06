98687dd1d3e9cd826e37828a352d2d4b6029c09d
AHSHAKER <ah172002.shaker172002@gmail.com>
2025-06-06 17:01:34 +0300

    Add deep linking support with custom scheme and update AndroidManifest; configure linking in AppNavigation

--- src/navigator/AppNavigation.tsx
+++ src/navigator/AppNavigation.tsx
@@ -4,11 +4,21 @@ import {useAuthStore} from '../store/AuthStore';
 import AuthStackNavigator from './AuthStackNavigator';
 import MainTabNavigator from './MainTabNavigator';
 
+const linking = {
+  prefixes: ['hzero://'],
+  config: {
+    screens: {
+      ProductList: 'products',
+      ProductDetails: 'product/:id',
+    },
+  },
+};
+
 function AppNavigation() {
   const isAuthenticated = useAuthStore(state => state.isAuthenticated);
 
   return (
-    <NavigationContainer>
+    <NavigationContainer linking={linking}>
       {isAuthenticated ? <MainTabNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
